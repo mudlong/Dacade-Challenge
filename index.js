38a99928807864799ad23a7ab80ae5e09733eabf
Shekinah Gonzwak <40487657+Shegz4real@users.noreply.github.com>
2019-11-05 09:26:50 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const contractSource = `
 contract Projectify =
 
   record project = {
-    id:int,
+    user:int,
     name: string,
     price:int,
     purchased:bool,
@@ -32,55 +32,31 @@ contract Projectify =
     state.projectLength
   
   payable stateful entrypoint addProject(_name:string, _price:int, _images:string, _documentation : string, _link : string ) =
-    let newProject = {id=getProjectLength() + 1, name=_name, price=_price, documentation = _documentation, link = _link, images=_images,purchased=false, owner=Call.caller, timestamp = Chain.timestamp}
+    let newProject = {user=getProjectLength() + 1, name=_name, price=_price, documentation = _documentation, link = _link, images=_images,purchased=false, owner=Call.caller, timestamp = Chain.timestamp}
     let index = getProjectLength() + 1
     put(state{projects[index] = newProject , projectLength  = index})
 
   
-  entrypoint get_project_by_index(index:int) : project = 
+  entrypoint getProject(index:int) : project = 
     switch(Map.lookup(index, state.projects))
       None => abort("Project does not exist with this index")
       Some(x) => x  
   
-  payable stateful entrypoint buyProject(_id:int)=
-    let buy_project = get_project_by_index(_id) // get the current Project with the id
+  payable stateful entrypoint tipProject(_user:int, tip:int)=
+    let tipProject = getProject(_user) // get the current Project with the user
     
-    let  _seller  = buy_project.owner : address
+    let  _seller  = tipProject.owner : address
     
-    require(buy_project.id > 0,abort("NOT A Project ID"))
+    require(tipProject.user> 0,abort("NOT A Project user"))
+  
+    Chain.spend(_seller, tip)
     
-    // require that there is enough AE in the transaction
-    require(Call.value >= buy_project.price,abort("You Don't Have Enough AE"))
-    
-    // require that the Project has not been purchased
-    
-    require(!buy_project.purchased,abort("PROJECT ALREADY PURCHASED"))
-    
-    // require that the buyer is not the seller
-    
-    require(_seller != Call.caller,"SELLER CAN'T PURCHASE HIS ITEM")
-    
-    // transfer ownership
-    
-    //buy_project.owner = Call.caller
-    
-    // mark as  purchased
-    
-    //buy_project.purchased = true 
-    
-    // update the Project
-    let updated_project = {id=buy_project.id, name=buy_project.name, price=buy_project.price, images=buy_project.images, documentation = buy_project.documentation, link = buy_project.link, purchased = true, owner=Call.caller, timestamp = buy_project.timestamp}
-    
-    put(state{projects[_id] = updated_project})
-    
-    // sends the amount
-    
-    Chain.spend(_seller, Call.value)
+    "Thank you for the tip"
   
     `;
 
 
-const contractAddress = 'ct_7UDAct1WNutBfm8A9Eqf644jSAZF6ecZ3M6dALWwHLwfbxb1R';
+const contractAddress = 'ct_spc6oNbPRdV7nXd7tt5vbZJq23KovaaQL5xZ9sV5Sfc6fPjZa';
 var ProjectArray = [];
 var client = null;
 var ProjectLength = 0;
@@ -178,31 +154,31 @@ window.addEventListener('load', async () => {
 
 
 
-// $("#body").on("click", ".voteBtn", async function (event) {
-//   $("#loading-bar-spinner").show();
-//   console.log("Just Clicked The vote Button")
+$("#body").on("click", ".voteBtn", async function (event) {
+  $("#loading-bar-spinner").show();
+  console.log("Just Clicked The vote Button")
 
 
 
-//   // const dataIndex = event.target.id
-//   dataIndex = ProjectArray.length
+  // const dataIndex = event.target.id
+  dataIndex = ProjectArray.length
 
 
-//   await contractCall('vote', [dataIndex], 0)
+  await contractCall('vote', [dataIndex], 0)
 
 
   
-//   // $("#votes").load(window.location.href + " #votes");
-//   location.reload(true)
+  // $("#votes").load(window.location.href + " #votes");
+  location.reload(true)
   
   
 
   
 
 
-//   renderProject();
-//   $("#loading-bar-spinner").hide();
-// });
+  renderProject();
+  $("#loading-bar-spinner").hide();
+});
 
 $('#regBtn').click(async function(){
   $("#loading-bar-spinner").show();
@@ -212,42 +188,45 @@ $('#regBtn').click(async function(){
   const Project_description = ($("#projectdescription").val());
   const Project_price = ($('#price').val());
   const Project_link = ($('#projectlink').val());
-  console.log("-------------------------------------")
-  console.log("Name:",Project_name)
-  console.log("images:",Project_images)
-  console.log("pric:",Project_price)
-  console.log("link:",Project_link)
 
-  const new_Project = await contractCall('addProject', [Project_name, Project_price, Project_images,Project_description, Project_link],parseInt(Project_price, 10));
-  console.log("SAVED TO THE DB", new_Project)
+
+  const newProject = await contractCall('addProject', [Project_name, Project_price, Project_images,Project_description, Project_link],parseInt(Project_price, 10));
+  
 
   ProjectArray.push({
-    id: new_Project.id,
-    images: new_Project.images,
+    id: newProject.id,
+    images: newProject.images,
 
-    name: new_Project.name,
-    description: new_Project.description,
-    link: new_Project.link,
-    price : new_Project.price
+    name: newProject.name,
+    description: newProject.description,
+    link: newProject.link,
+    price : newProject.price
   })
 
 
   renderProject();
-  
-    //This will clear the value in all scenarious
-    var name_input = document.getElementById("Username")
-        name_input.value =""
-    var image_input = document.getElementById("imagelink")
-        url_input.value =""
-    var image_input = document.getElementById("projectdescription")
-       image_input.value = ""
-    var image_input = document.getElementById("price")
-       image_input.value = ""
-    var image_input = document.getElementById("projectlink")
-       image_input.value = ""
-  // e.preventDefault();
+ 
 
   $("#loading-bar-spinner").hide();
   location.reload(true)
+
+});
+
+$('#body').on('click', '#tipbutton', async function(event){
+  $("#loading-bar-spinner").show();
+
+  dataIndex = ProjectArray.length
+
+  const tipValue = ($('#tipValue').val());
+
+  
+
+
+  await contractCall('tipProject', [dataIndex, tipValue], 0)
+
+  console.log("Tipped successfully")
+
+
+  $("#loading-bar-spinner").hide();
 
 });
